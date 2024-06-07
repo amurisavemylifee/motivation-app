5bb941dc9538b158f1c3cd6f67c79a9846fe7b9b
Taras Shashurin <taras.shashurin.work@gmail.com>
2024-06-08 01:34:14 +0300

    Добавил базовый сервер

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("app started");
+import express from "express";
+
+import { DebugController } from "controllers/DebugController";
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  DebugController.log(`Server is running on port ${PORT}`);
+});
